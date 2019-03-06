dd13e6bab3d7e2c1ab14d43e94cf871facd4c192
naval-ucreate <naval@ucreate.co.in>
2019-03-06 15:47:26 +0530

    Scale Image Example

---
+++ app/Http/Controllers/FileUploadController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileUploadController extends Controller
+{
+    var $path  = "";
+    public function __construct(){
+        $this->path = public_path('/images');
+    }
+    public function index(){
+        return view('fileupload');
+    }
+     function scaleImage(Array $parms)
+    {        
+        $input          =   $this->path."/".$parms['name'];
+        $output_image   =   time().'_'.$parms['scale_size'].'.'.$parms['extension'];
+        $output         =   $this->path."/".$output_image;
+        exec('ffmpeg -i '.$input.' -vf scale='.$parms['scale_size'].':-1 '.$output);
+        $data['input_image']    =   $parms['name'];
+        $data['output_image']   =   $output_image;
+        return $data;
+    }
+    public function fileUpload(Request $request){
+
+       $this->validate($request, [
+            'file_upload' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);     
+        $image              = $request->file('file_upload');
+        $parms['extension'] = $image->getClientOriginalExtension();
+        $parms['name']      = time().'.'.$image->getClientOriginalExtension();
+        $destinationPath    = $this->path;
+        if($image->move($destinationPath, $parms['name']))
+        {
+            $parms['scale_size']    = $request->input('scale_size');
+            $image_data             = $this->scaleImage($parms);
+            return back()->with('success','Image Upload successful')->with( ['image_data' => $image_data] );
+        }
+        return back()->with('error','There is some issue!');
+    }
+
+}
---
+++ resources/views/fileupload.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<body>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+<div class="container">
+<div class="row">
+        <div class="col-md-12 text-center">
+
+        @if ($message = Session::get('success'))
+        <div class="alert alert-success alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>	
+        <strong>{{ $message }}</strong>
+        </div>
+        @endif
+
+
+        @if ($message = Session::get('error'))
+        <div class="alert alert-danger alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>	
+        <strong>{{ $message }}</strong>
+        </div>
+        @endif
+
+
+        @if ($message = Session::get('warning'))
+        <div class="alert alert-warning alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>	
+        <strong>{{ $message }}</strong>
+        </div>
+        @endif
+
+
+        @if ($message = Session::get('info'))
+        <div class="alert alert-info alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>	
+        <strong>{{ $message }}</strong>
+        </div>
+        @endif
+
+
+        @if ($errors->any())
+        <div class="alert alert-danger">
+        <button type="button" class="close" data-dismiss="alert">×</button>	
+        Please check the form below for errors
+        </div>
+        @endif
+
+        <form action="{{ route('file_upload') }}" method="post" enctype="multipart/form-data">
+        {{ csrf_field() }}
+            <div class="form-group">
+            <label for="Scale Size">Scale Size:</label>
+                <select name='scale_size'>
+                   <?php for($scale_size=100;$scale_size<1100;$scale_size+=100) { ?>
+                    <option value="{{$scale_size}}">{{$scale_size}}</option>
+                   <?php }  ?>
+                </select>
+            </div>
+            <div class="form-group">
+            <label for="email">Upload Image:</label>
+            <input type="file" class="form-control" id="file_upload" name="file_upload">
+            </div>       
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>    
+        </div>        
+    </div>
+            @if ($image_data = Session::get('image_data'))
+            <p>Input Image:</p>
+            <img src="images/{{ $image_data['input_image'] }}">
+            <br/>
+            <p>Output Image</p>
+            <img src="images/{{ $image_data['output_image'] }}">
+            @endif
+</div>
+</body>
+</html>
